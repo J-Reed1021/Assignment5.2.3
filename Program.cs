e54986a773eb35d91b199151895426dcf07dc5d5
joshu <joshu@REEDPC>
2024-06-05 09:24:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment5._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NaturalNumbers(10);
+
+            void NaturalNumbers(int n)
+            {
+                if (n == 1)
+                {
+                    Console.Write(n);
+
+                }
+                else
+                {
+                    Console.Write(" " + n);
+                    NaturalNumbers(n - 1);
+                    
+                }
+            }
+        }
+    }
+}
